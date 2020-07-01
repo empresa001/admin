7e8ce03be150f68254fe708b4282af373ace3f79
dgmr.tabu <dgmr.tabu@gmail.com>
2020-06-30 20:47:53 -0400

    Seccion 13 inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // Rutas
 import { AppRoutingModule } from './app-routing.module';
@@ -28,7 +28,8 @@ import { ServiceModule } from './services/service.module';
     AppRoutingModule,
     PagesModule,
     ServiceModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
