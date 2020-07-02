544a408387afdce94a79dcd2f02d9f0b62c6b5b3
dgmr.tabu <dgmr.tabu@gmail.com>
2020-07-02 19:08:36 -0400

    Seccion 13

--- src/app/login/register.component.ts
+++ src/app/login/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import swal from 'sweetalert';
+
 
 declare function init_plugins();
 
@@ -14,6 +16,24 @@ export class RegisterComponent implements OnInit {
 
   constructor() { }
 
+  sonIguales(campo1: string, campo2: string){
+
+    return(group: FormGroup) => {
+
+      let pass1 = group.controls[campo1].value;
+      let pass2 = group.controls[campo2].value;
+
+        // tslint:disable-next-line: align
+        if ( pass1 === pass2 ) {
+          return null;
+        }
+
+      return{
+        sonIguales: true
+      };
+    };
+  }
+
   ngOnInit(): void {
     init_plugins();
 
@@ -24,10 +44,27 @@ export class RegisterComponent implements OnInit {
       password2: new FormControl(null, Validators.required),
       condiciones: new FormControl(false),
 
+    }, {validators: this.sonIguales('password','password2')});
+
+    this.forma.setValue({
+      nombre: 'Test',
+      correo: 'test@test.com',
+      password: '123456',
+      password2: '123456',
+      condiciones: true
     });
   }
 
   registrarUsuario(){
+
+    if ( this.forma.invalid ) {
+      return;
+    }
+    if ( !this.forma.value.condiciones ) {
+      //console.log('Debe de aceptar las condiciones de uso');
+      swal("Importante", "Debe de aceptar las condiciones de uso", "warning");
+      return;
+    }
     console.log(this.forma.value);
   }
 
